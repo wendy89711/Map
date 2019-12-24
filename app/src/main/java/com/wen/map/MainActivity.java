7b5fab7f5f8740a15c8bac568f5b70f2e7a24098
wupeiwen <wendy89711@gmail.com>
2019-12-24 14:26:49 +0800

    做到要箭頭準備加圈圈

---
+++ app/src/main/java/com/wen/map/MainActivity.java
@@ -0,0 +1,43 @@
+package com.wen.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+
+    }
+
+    private void findViews() {
+        findViewById(R.id.arrow_up).setOnClickListener(this);
+        findViewById(R.id.arrow_right).setOnClickListener(this);
+        findViewById(R.id.arrow_down).setOnClickListener(this);
+        findViewById(R.id.arrow_left).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.arrow_up:
+                Log.d("MainActivity","onClick:UP");
+                break;
+            case R.id.arrow_right:
+                Log.d("MainActivity","onClick:RIGHT");
+                break;
+            case R.id.arrow_down:
+                Log.d("MainActivity","onClick:DOWN");
+                break;
+            case R.id.arrow_left:
+                Log.d("MainActivity","onClick:LEFT");
+                break;
+        }
+    }
+}
