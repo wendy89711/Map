ef86b6b7155d4af65c1f1c81e67ce9bf549ee747
wupeiwen <wendy89711@gmail.com>
2019-12-26 23:13:52 +0800

    重新做一個

--- app/src/main/java/com/wen/map/MainActivity.java
+++ app/src/main/java/com/wen/map/MainActivity.java
@@ -6,49 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private GameView gameView;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViews();
 
-    }
 
-    private void findViews() {
-        findViewById(R.id.arrow_up).setOnClickListener(this);
-        findViewById(R.id.arrow_right).setOnClickListener(this);
-        findViewById(R.id.arrow_down).setOnClickListener(this);
-        findViewById(R.id.arrow_left).setOnClickListener(this);
-        gameView = findViewById(R.id.game);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.arrow_up:
-                Log.d("MainActivity","onClick:UP");
-                gameView.setPosY(gameView.getPosY()-50);
-                gameView.invalidate();
-                break;
-            case R.id.arrow_down:
-                Log.d("MainActivity","onClick:DOWN");
-                gameView.setPosY(gameView.getPosY()+50);
-                gameView.invalidate();
-                break;
-            case R.id.arrow_left:
-                Log.d("MainActivity","onClick:LEFT");
-                gameView.setPosX(gameView.getPosX()-50);
-                gameView.invalidate();
-                break;
-            case R.id.arrow_right:
-                Log.d("MainActivity","onClick:RIGHT");
-                gameView.setPosX(gameView.getPosX()+50);
-                gameView.invalidate();
-                break;
-        }
     }
 }
